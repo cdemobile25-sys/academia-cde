1dc89eb0020df27307eb9fdf86de1e5508432ab2
keolazarte-lgtm <keolazarte@gmail.com>
2026-05-05 10:46:19 -0300

    Creando carpeta gracias nuevamente

--- app/gracias/page.tsx
+++ app/gracias/page.tsx
@@ -1,8 +1,1 @@
-export default function GraciasPage() {
-  return (
-    <main style={{ padding: 20, textAlign: 'center', fontSize: 24, color: 'green' }}>
-      <h1>¡FUNCIONA!</h1>
-      <p>Si ves esto, la página está bien creada.</p>
-    </main>
-  );
-}
+export default function Gracias() { return <main style={{padding:20}}>GRACIAS - FUNCIONA</main> }
