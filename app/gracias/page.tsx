85a4c468050a1cb7608a09272ddb2f34ebd83a94
keolazarte-lgtm <keolazarte@gmail.com>
2026-05-05 13:35:57 -0300

    Trigger rebuild gracias

--- app/gracias/page.tsx
+++ app/gracias/page.tsx
@@ -1,1 +1,4 @@
-export default function Gracias() { return <main style={{padding:20}}>GRACIAS - FUNCIONA</main> }
+// Prueba de build
+export default function Gracias() { 
+  return <main style={{padding:20, color:'green', fontSize:30}}>GRACIAS - FUNCIONA</main> 
+}
